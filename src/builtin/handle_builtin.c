e3c58eadec62b32e19a076e81310e2edc3c3ca74
Pierre-Edouard Oriou <poriou@bess-f3r1s11.clusters.42paris.fr>
2024-05-23 10:13:46 +0200

    pushing for test

--- src/builtin/handle_builtin.c
+++ src/builtin/handle_builtin.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   handle_builtin.c                                   :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: stouitou <stouitou@student.42.fr>          +#+  +:+       +#+        */
+/*   By: poriou <poriou@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/21 12:25:04 by stouitou          #+#    #+#             */
-/*   Updated: 2024/05/22 15:49:47 by stouitou         ###   ########.fr       */
+/*   Updated: 2024/05/23 10:07:40 by poriou           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,4 +16,6 @@ void	handle_builtin(t_entry *entry, t_exe *exe, char *command)
 {
 	if (ft_strcmp(command, "exit") == 0)
 		handle_exit(entry, exe, exe->cmd);
+	if (ft_strcmp(command, "echo") == 0)
+		handle_echo(entry, exe, exe->cmd);
 }
