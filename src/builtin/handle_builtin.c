e5dc1c324d530afad34d14ebfaf527457dd77c09
Sarah Touitou <stouitou@bess-f3r1s9.clusters.42paris.fr>
2024-05-23 13:17:29 +0200

    galere

--- src/builtin/handle_builtin.c
+++ src/builtin/handle_builtin.c
@@ -6,18 +6,18 @@
 /*   By: stouitou <stouitou@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/23 12:57:59 by stouitou          #+#    #+#             */
-/*   Updated: 2024/05/23 12:58:00 by stouitou         ###   ########.fr       */
+/*   Updated: 2024/05/23 13:14:27 by stouitou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-void	handle_builtin(t_entry *entry, t_exe *exe, char *command)
+void	handle_builtin(t_exe *exe, char *command)
 {
 	if (ft_strcmp(command, "exit") == 0)
-		handle_exit(entry, exe, exe->cmd);
+		handle_exit(exe, exe->cmd);
 	if (ft_strcmp(command, "export") == 0)
-		handle_export(entry, exe, exe->cmd, exe->env);
+		handle_export(exe, exe->cmd, exe->env);
 	if (ft_strcmp(command, "env") == 0)
 		handle_env(exe->cmd, exe->env);
 	if (ft_strcmp(command, "echo") == 0)
