b71fed2f7a9e090518c831c84ef16a233e42d849
Sarah Touitou <stouitou@bess-f3r1s9.clusters.42paris.fr>
2024-05-23 10:25:07 +0200

    add env builtin

--- src/builtin/handle_builtin.c
+++ src/builtin/handle_builtin.c
@@ -6,7 +6,7 @@
 /*   By: stouitou <stouitou@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/21 12:25:04 by stouitou          #+#    #+#             */
-/*   Updated: 2024/05/22 15:49:47 by stouitou         ###   ########.fr       */
+/*   Updated: 2024/05/23 10:24:37 by stouitou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,4 +16,6 @@ void	handle_builtin(t_entry *entry, t_exe *exe, char *command)
 {
 	if (ft_strcmp(command, "exit") == 0)
 		handle_exit(entry, exe, exe->cmd);
+	if (ft_strcmp(command, "env") == 0)
+		handle_env(entry, exe, exe->cmd);
 }
