878170a6687c8ed26026eb8b5a2a17da2c9140fa
Pierre-Edouard Oriou <poriou@bess-f4r1s5.clusters.42paris.fr>
2024-06-04 14:40:43 +0200

    pushing pwd and cd early version

--- src/builtin/handle_cd_in_parent.c
+++ src/builtin/handle_cd_in_parent.c
@@ -6,7 +6,7 @@
 /*   By: poriou <poriou@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/28 10:52:05 by poriou            #+#    #+#             */
-/*   Updated: 2024/05/28 14:13:18 by poriou           ###   ########.fr       */
+/*   Updated: 2024/06/04 14:37:54 by poriou           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,21 +15,36 @@
 static void	free_cd_b4_return(t_entry *entry, t_exe *exe, char *err, int status)
 {
 	free_exe(exe);
-	ft_fprintf(2, err);
+	ft_putendl_fd(err, 2);
 	entry->status = status;
 }
 
-static bool	key_exists_in_env(char *key, t_env *env)
+static bool	too_many_args(t_entry *entry, t_exe *exe, char **cmd)
 {
-	if (!env)
-		return (false);
-	while (env)
+	if (ft_str_array_len(cmd) > 2)
 	{
-		if (ft_strcmp(env->key, key) == 0)
-			return (true);
-		env = env->next;
+		free_cd_b4_return(entry, exe, "cd: too many arguments", 1);
+		return (true);
 	}
 	return (false);
+}
+
+static char	*extract_key_value(t_env *env, char *key)
+{
+	t_env	*cur;
+	char	*value;
+
+	cur = env;
+	while (cur)
+	{
+		if (ft_strcmp(cur->key, key) == 0)
+		{
+			value = cur->value;
+			return (value);
+		}
+		cur = cur->next;
+	}
+	return (NULL);
 }
 
 static t_env	*get_in_env(t_env *env, char *key)
@@ -48,93 +63,114 @@ static t_env	*get_in_env(t_env *env, char *key)
 	return (NULL);
 }
 
-static void	upd_pwd_env(t_entry *entry, t_exe *exe, t_env *env, char *path)
+static char	*find_cwd(void)
 {
-	t_env	*pwd;
-	t_env	*oldpwd;
-	bool	pwd_exists;
-	bool	oldpwd_exists;
+	char	*pwd;
+	char	*buf;
+	int		size;
 
-	// if (!key_exists_in_env("PWD", env))
-	// {
-	// 	oldpwd = get_in_env("OLDPWD", env);
-	// 	if (!oldpwd)
-	// 		return ;
-	// 	free(oldpwd->value);
-	// 	oldpwd->value = ft_strdup("");
-	// 	if (!oldpwd->value && errno == ENOMEM)
-	// 		free_token_and_exit(entry, strerror(errno), path, EXIT_FAILURE);
-	// }
-	pwd = get_in_env("PWD", env);
-	if (!pwd)
-		pwd_exists = false;
-	else
-		pwd_exists = true;
-	oldpwd = get_in_env("OLDPWD", env);
-	if (!oldpwd)
-		oldpwd_exists = false;
-	else
-		oldpwd_exists = true;
-	if (oldpwd_exists && pwd_exists)
+	buf = NULL;
+	size = 0;
+	while (1)
 	{
-		free(oldpwd->value);
-		oldpwd->value = ft_strdup(pwd->value);
-		if (!pwd->value && errno == ENOMEM)
-			free_token_and_exit(entry, strerror(errno), path, EXIT_FAILURE);
+		pwd = getcwd(buf, size);
+		if (!pwd && errno == ERANGE)
+		{
+			size++;
+			continue ;
+		}
+		else if (!pwd)
+			return (NULL);
+		break ;
 	}
-	if (oldpwd_exists && !pwd_exists)
-	{
-		free(oldpwd->value);
-		oldpwd->value = ft_strdup("");
-		if (!oldpwd->value && errno == ENOMEM)
-			free_token_and_exit(entry, strerror(errno), path, EXIT_FAILURE);
-	}
-	if (pwd_exists)
-	{
-		free(pwd->value);
-		pwd->value = ft_strdup(path);
-		if (!pwd->value && errno == ENOMEM)
-			free_token_and_exit(entry, strerror(errno), path, EXIT_FAILURE);
-	}
-	// if (key_exists_in_env("OLDPWD", env))
-	// {
-	// 	oldpwd = get_in_env("OLDPWD", env);
-	// 	pwd = get_in_env("PWD", env);
-	// 	free(oldpwd->value);
-	// 	oldpwd->value = ft_strdup(path);
-	// 	if (!pwd->value)
-	// 		free_token_and_exit(entry, strerror(errno), path, EXIT_FAILURE);
-	// }
-	// if (key_exists_in_env("PWD", env))
-	// {
-	// 	pwd = find_key_in_env("PWD", env);
-	// 	free(pwd->value);
-	// 	pwd->value = ft_strdup(path);
-	// 	if (!pwd->value)
-	// 		free_token_and_exit(entry, strerror(errno), path, EXIT_FAILURE);
-	// }
+	return (pwd);
 }
 
-static bool	too_many_args(t_entry *entry, t_exe *exe, char **cmd)
+static void	upd_env_pwd(t_env *env)
 {
-	if (ft_str_array_len(cmd) > 2)
+	char	*cwd;
+	t_env	*oldpwd;
+	t_env	*pwd;
+
+	cwd = find_cwd();
+	oldpwd = get_in_env(env, "OLDPWD");
+	pwd = get_in_env(env, "PWD");
+	ft_printf("before upd_env_pwd\n");
+	ft_printf("pwd = %s\n", pwd->value);
+	if (oldpwd)
 	{
-		free_cd_b4_return(entry, exe, "cd: too many arguments\n", 1);
-		return (true);
+		if (pwd)
+		{
+			free(oldpwd->value);
+			oldpwd->value = pwd->value;
+		}
 	}
-	return (false);
+	if (pwd)
+	{
+		if (!oldpwd)
+			free(pwd->value);
+		pwd->value = cwd;
+	}
+	ft_printf("after upd_env_pwd\n");
+	ft_printf("oldpwd = %s\n", oldpwd->value);
+	ft_printf("pwd = %s\n", pwd->value);
+}
+
+static char	*handle_hyphen(t_entry *entry, t_exe *exe, t_env *env)
+{
+	t_env	*oldpwd;
+
+	oldpwd = get_in_env(env, "OLDPWD");
+	if (!oldpwd)
+	{
+		free_cd_b4_return(entry, exe, "cd: OLDPWD not set", 1);
+		entry->status = 1;
+		return (NULL);
+	}
+	return (oldpwd->value);
+}
+
+static char	*get_path(t_entry *entry, t_exe *exe, t_env *env, char **cmd)
+{
+	char	*path;
+
+	path = NULL;
+	if (ft_str_array_len(cmd) == 1)
+	{
+		path = extract_key_value(env, "HOME");
+		if (!path)
+		{
+			free_cd_b4_return(entry, exe, "cd: HOME not set", 1);
+			entry->status = 1;
+			return (NULL);
+		}
+	}
+	else
+		path = cmd[1];
+	if (ft_strcmp(path, "-") == 0)
+	{
+		path = handle_hyphen(entry, exe, env);
+		if (!path)
+		{
+			return (NULL);
+		}
+	}
+	return (path);
+}
+
+static void	change_directory(t_entry *entry, t_exe *exe, char *path)
+{
+	if (chdir(path) == -1)
+	{
+		free_cd_b4_return(entry, exe, strerror(errno), 1);
+		return ;
+	}
 }
 
 int	handle_cd_in_parent(t_entry *entry, t_exe *exe, t_env *env, char **cmd)
 {
-	t_env	*oldpwd;
-	t_env	*pwd;
-	int		exit_status;
-	int		size;
-	char	*old_path;
 	char	*path;
 
-	(void)env;
 	if (!cmd || exe->blocks > 1 || ft_strcmp(cmd[0], "cd") != 0)
 		return (0);
 	if (!get_files_fd_for_exit(exe, exe->files))
@@ -145,65 +181,11 @@ int	handle_cd_in_parent(t_entry *entry, t_exe *exe, t_env *env, char **cmd)
 	}
 	if (too_many_args(entry, exe, cmd))
 		return (1);
-	oldpwd = get_in_env(env, "OLDPWD");
-	pwd = get_in_env(env, "PWD");
-	upd_oldpwd(entry, env, oldpwd, pwd);
-	entry->status = exit_status;
+	path = get_path(entry, exe, env, cmd);
+	if (!path)
+		return (1);
+	// ft_printf("after get_path, path = %s\n", path);
+	change_directory(entry, exe, path);
+	upd_env_pwd(env);
 	return (1);
 }
-
-// int	handle_cd_in_parent(t_entry *entry, t_exe *exe, t_env *env, char **cmd)
-// {
-// 	int		exit_status;
-// 	int		size;
-// 	char	*old_path;
-// 	char	*path;
-
-// 	(void)env;
-// 	if (!cmd || exe->blocks > 1 || ft_strcmp(cmd[0], "cd") != 0)
-// 		return (0);
-// 	if (!get_files_fd_for_exit(exe, exe->files))
-// 	{
-// 		free_exe(exe);
-// 		entry->status = 1;
-// 		return (1);
-// 	}
-// 	if (ft_str_array_len(cmd) > 2)
-// 	{
-// 		free_cd_b4_return(entry, exe, "cd: too many arguments\n", 1);
-// 		return (1);
-// 	}
-// 	exit_status = 0;
-// 	size = 0;
-// 	old_path = getenv("PWD");
-// 	path = NULL;
-// 	if (!cmd[1])
-// 	{
-// 		path = getenv("HOME");
-// 		if (!path)
-// 		{
-// 			free_cd_b4_return(entry, exe, "cd: HOME not set\n", 1);
-// 			return (1);
-// 		}
-// 		if (chdir(path) == -1)
-// 			perror("chdir");
-// 		upd_pwd_env(entry, exe, env, path);
-// 		entry->status = exit_status;
-// 		return (1);
-// 	}
-// 	if (ft_strcmp(cmd[1], "-") == 0)
-// 	{
-// 		path = getenv("OLDPWD");
-// 		if (!path)
-// 		{
-// 			free_cd_b4_return(entry, exe, "cd: OLDPWD not set\n", 1);
-// 			return (1);
-// 		}
-// 		if (chdir(path) == -1)
-// 			perror("chdir");
-// 		entry->status = exit_status;
-// 		return (1);
-// 	}
-// 	entry->status = exit_status;
-// 	return (1);
-// }
